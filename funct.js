09bf2504938e0b61ff3410cf5aec323c1e579e66
john alain <johnalain572@gmail.com>
2023-11-04 11:00:49 +0200

    funct.

---
+++ funct.js
@@ -0,0 +1,32 @@
+// function add(a,b){
+//     return a+b
+// }{}
+//  add(2,3);
+ const ad = (a,b) =>  a*b ; //if u have one line inside curly braces u can eliminate it and eliminite return as well
+    
+ console.log(ad(5,5));
+// // ad(3,4);
+// document.write(ad);
+function sayHello(userName,age){
+    return `hello ${userName} my age is : ${age}`;
+}
+console.log(sayHello("michel",32));
+const ude = (x,y) => x + y;
+console.log(ude(10,20));
+
+function outer(){
+    console.log('outer');
+    function inner(){
+        console.log('inner');
+    }
+    console.log(inner());
+}
+function outer(){
+    console.log('outer');
+    function inner(){
+        console.log('inner');
+    }
+    inner();
+}
+outer();
+// u can't call nested function only outer function print both
