b01821ba4d06d00382a46c176196f7270e96da54
john alain <johnalain572@gmail.com>
2023-11-29 18:15:44 +0200

    weather

---
+++ asyncawaitjs.js
@@ -0,0 +1,29 @@
+// https://www.youtube.com/watch?v=spvYqO_Kp9Q
+function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(46);
+    }, 1);
+  });
+}
+async function start() {
+  const result = await getData();
+  console.log(result);
+}
+start();
+
+// async function start() {
+//   const data = await fetch("https://openweathermap.org/api");
+// //   invalid url for get data
+
+//   const result = await Data.json();
+//   console.log(result);
+// }
+// start();
+
+async function start2() {
+  getData().then((result) => {
+    console.log(result);
+  });
+}
+start2();
