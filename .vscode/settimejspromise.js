005c0bb344acb7fe6dd3e69f12f134b3332cf1f6
john alain <johnalain572@gmail.com>
2023-11-30 14:51:48 +0200

    promise3

--- .vscode/settimejspromise.js
+++ .vscode/settimejspromise.js
@@ -1,44 +1,53 @@
 // https://www.youtube.com/watch?v=7s6aMzXyt3g
-// new Promise((resolve, reject) => {
-//   if (isSuccess) {
-//     resolve();
-//   } else {
-//     reject();
-//   }
-// });
-// then(() => {
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    document.getElementById("title1").style.visibility = "visible";
-    resolve();
-  }, 1000);
-})
-  // });
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        document.getElementById("title2").style.visibility = "visible";
-        resolve();
-      }, 1000);
-    });
-  })
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        document.getElementById("title3").style.visibility = "visible";
-        resolve();
-      }, 1000);
-    });
-  })
+// this file in github-->repository prototypejs-->.vscode
 
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
+//new method using promise javascript
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     document.getElementById("title1").style.visibility = "visible";
+//     resolve();
+//   }, 1000);
+// })
+//   // });
+//   .then(() => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         document.getElementById("title2").style.visibility = "visible";
+//         resolve();
+//       }, 1000);
+//     });
+//   })
+//   .then(() => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         document.getElementById("title3").style.visibility = "visible";
+//         resolve();
+//       }, 1000);
+//     });
+//   })
+
+//   .then(() => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         document.getElementById("title4").style.visibility = "visible";
+//         resolve();
+//       }, 1000);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("this is an error" + error);
+//   });
+
+//old method using promise javascript
+setTimeout(function () {
+  document.getElementById("title1").style.visibility = "visible";
+  setTimeout(function () {
+    document.getElementById("title2").style.visibility = "visible";
+    setTimeout(function () {
+      document.getElementById("title3").style.visibility = "visible";
+      setTimeout(function () {
         document.getElementById("title4").style.visibility = "visible";
-        resolve();
       }, 1000);
-    });
-  })
-  .catch((error) => {
-    console.log("this is an error" + error);
-  });
+    }, 1000);
+  }, 1000);
+}, 1000);
