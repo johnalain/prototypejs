638c13e92cdf404485dbd69c4007678749950352
john alain <johnalain572@gmail.com>
2023-12-01 16:50:17 +0200

    dom3

---
+++ .vscode/domvar.js
@@ -0,0 +1,47 @@
+// https://www.facebook.com/photo?fbid=285912564437493&set=pcb.6731428933652780
+var num = [1, 2, 3, 4, "josephine", "amal", "rita"];
+// num.push(5);
+// console.log(num);
+// num.at(1);
+// console.log(num.at(1));
+// num.pop();
+// console.log(num);
+// num.fill(1);
+// console.log(num);
+// num.shift();
+// console.log(num);
+// num.unshift(5);
+// num.reverse();
+
+// console.log(num.includes(2));
+
+// console.log(num.unshift(5));
+// console.log(num.map((item) => item * 2));
+// console.log(num.map((item) => item > 2));
+// num.find((item) => item > 2);
+// console.log(num.find((item) => item > 2));
+// num.every((item) => item > 0);
+
+// num.findIndex((item) => item === 2);
+// console.log(num.findIndex((item) => item === 2));
+// num.reduce((prev, curr) => prev + curr, 0);
+// console.log(num.reduce((prev, curr) => prev + curr, 0));
+// num.toString();
+// console.log(num.toString());
+// num.join("*");
+// console.log(num.join("*"));
+// num.splice(2, 0, "i", "p");
+// num.slice(1, 4);
+// num.sort();
+console.log(num.sort());
+x.sort(function (a, b) {
+  a - b;
+});
+x.sort(function (a, b) {
+  a - b;
+});
+x.sort(function (a, b) {
+  return 0.5 - math.random();
+});
+
+sort();
