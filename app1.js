f235d76c50a1004e72590c1f41387aab7ca83273
john alain <johnalain572@gmail.com>
2024-01-02 08:25:27 +0200

    firstcommit

---
+++ app1.js
@@ -0,0 +1,11 @@
+const john = 'john'
+const  peter= 'peter'
+
+// const sayhi =(name) =>{
+//     console.log('hello there ${name}')
+// }
+ array.forEach(element => {
+    sayhi('josephine')
+});
+sayhi(john)
+sayhi(peter)
